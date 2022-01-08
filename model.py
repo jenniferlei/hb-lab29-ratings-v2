dc7b5bb4910bda5b570c6b18194450c5ef2bd010
Jennifer Lei <jenniferlei1928@gmail.com>
2022-01-08 21:26:28 +0000

    Set up SQLAlchemy relationships between ratings, movies, and users

--- model.py
+++ model.py
@@ -18,6 +18,8 @@ class User(db.Model):
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
 
+    #ratings = a list of Rating objects
+
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email}>'
 
@@ -34,6 +36,8 @@ class Movie(db.Model):
     release_date = db.Column(db.DateTime)
     poster_path = db.Column(db.String)
 
+    #ratings = a list of Rating objects
+
     def __repr__(self):
         return f'<Movie movie_id={self.movie_id} title={self.title}>'
 
@@ -49,6 +53,10 @@ class Rating(db.Model):
     score = db.Column(db.Integer)
     movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+
+    movie = db.relationship("Movie", backref="ratings")
+    user = db.relationship("User", backref="ratings")
+
 
     def __repr__(self):
         return f'<Rating rating_id={self.rating_id} score={self.score}>'
