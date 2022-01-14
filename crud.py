53cc83472ffb72030b7d66cc2957f6741f41a655
Jennifer Lei <jenniferlei1928@gmail.com>
2022-01-14 04:10:51 +0000

    Added MVP features: view list of all movies, view details of movie, view list of all users, view details of user

--- crud.py
+++ crud.py
@@ -19,6 +19,30 @@ def create_movie(title, overview, release_date, poster_path):
     return movie
 
 
+def get_movies():
+    """Return all movies."""
+
+    return Movie.query.all()
+
+
+def get_movie_by_id(movie_id):
+    """Return movie by id.""" 
+
+    return Movie.query.get(movie_id)
+
+
+def get_users():
+    """Return all users."""
+
+    return User.query.all()
+
+
+def get_user_by_id(user_id):
+    """Return user by id.""" 
+
+    return User.query.get(user_id)
+
+
 def create_rating(user, movie, score):
     """Create and return a new rating."""
 
