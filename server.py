f4bafb87d8e45f90861abed24d6abb277702da9d
Jennifer Lei <jenniferlei1928@gmail.com>
2022-01-15 21:04:21 +0000

    Added MVP: create account, login, rate movies

--- server.py
+++ server.py
@@ -38,6 +38,62 @@ def show_movie(movie_id):
     return render_template("movie_details.html", movie=movie)
 
 
+@app.route("/users", methods=["POST"])
+def register_user():
+    """Create a new user."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = crud.get_by_email(email)
+    if user:
+        flash("Cannot create an account with that email. Try again.")
+    else:
+        user = crud.create_user(email,password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created. Please log in.")
+
+    return redirect("/")
+
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """Process user login"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = crud.get_by_email(email)
+    if not user or user.password != password:
+        flash("The email or password is incorrect.")
+    else:
+        session["user_email"] = user.email
+        flash("Welcome back, {user.email}!")
+
+    return redirect("/")
+
+@app.route("/movies/<movie_id>/ratings", methods=["POST"])
+def create_rating(movie_id):
+
+    logged_in_email = session.get("user_email")
+    rating_score = request.form.get("rating")
+
+    if logged_in_email is None:
+        flash("You must log in to rate a movie.")
+    elif not rating_score:
+        flash("Error: you didn't select a score for your rating.")
+    else:
+        user = crud.get_user_by_email(logged_in_email)
+        movie = crud.get_movie_by_id(movie_id)
+
+        rating = crud.create_rating(user, movie, int(rating_score))
+        db.session.add(rating)
+        db.session.commit()
+
+        flash(f"You rated this movie {rating_score} out of 5.")
+
+    return redirect(f"/movies/{movie_id}")   
+
+
 @app.route("/users")
 def all_users():
     """View all users."""
